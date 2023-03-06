f4860d7d7753da15daf9f18081a6df9c32961a57
jaworskiwojciech <jaworski.wojciech94@gmail.com>
2023-03-06 22:07:37 +0100

    second task done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,37 @@
+import '../css/common.css';
+import player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const TIME_KEY = 'videoplayer-current-time';
+const iframe = document.querySelector('iframe');
+
+const vimeoPlayer = new player(iframe);
+
+const onPlay = function (data) {
+  const strigifyData = JSON.stringify(data);
+  localStorage.setItem(TIME_KEY, strigifyData);
+};
+vimeoPlayer.on('timeupdate', throttle(onPlay, 100));
+
+function resumePlayback() {
+  const storedData = JSON.parse(localStorage.getItem(TIME_KEY));
+  if (storedData === null) {
+    return;
+  }
+  const paused = storedData['seconds'];
+  if (paused) {
+    vimeoPlayer
+      .setCurrentTime(paused)
+      .then(function (seconds) {})
+      .catch(function (error) {
+        switch (error.name) {
+          case 'RangeError':
+            break;
+          default:
+            break;
+        }
+      });
+  }
+  console.log(storedData['seconds']);
+}
+resumePlayback();
